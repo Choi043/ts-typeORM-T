ed71138590e33ae918d1cb9cc3bb9d4f38d06734
Choi043 <rqdda11@gmail.com>
2022-03-29 19:24:18 +0900

    add Entity

--- src/types/types.service.ts
+++ src/types/types.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Types } from './entity/types.entity';
 
 @Injectable()
-export class TypesService {}
+export class TypesService {
+    constructor(
+        @InjectRepository(Types)
+        private typesRepository: Repository<Types>,
+    ) {}
+
+    findAll(): Promise<Types[]> {
+        return this.typesRepository.find();
+    }
+
+    findOne(id: number): Promise<Types> {
+        return this.typesRepository.findOne();
+    }
+
+    async create(types: Types): Promise<void> {
+        await this.typesRepository.save(types);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.typesRepository.delete(id);
+    }
+
+    
+}
