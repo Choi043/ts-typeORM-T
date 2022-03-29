5c4c15fe9368181bb7662203ce94762ae8d43d72
ChoiYH43 <rqdda11@blockodyssey.io>
2022-03-29 23:50:48 +0900

    add controller

--- src/types/types.controller.ts
+++ src/types/types.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Types } from './entity/types.entity';
+import { TypesService } from './types.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('types')
-export class TypesController {}
+export class TypesController {
+    constructor(private typesService: TypesService) {
+    }
+    @Get()
+    findAll(): Promise<Types[]> {
+        return this.typesService.findAll();
+    }
+    @Get(':id')
+    findOne(@Param('id')id: number): Promise<Types> {
+        return this.typesService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() types: Types) {
+        return this.typesService.create(types);
+    }
+}
