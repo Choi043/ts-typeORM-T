729054f34390b2c4f2f02f968e9a1eab64a50864
Choi043 <rqdda11@gmail.com>
2022-04-05 11:15:58 +0900

    add authModule and SignIn

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { Types } from './types/entity/types.entity';
 import { TypesModule } from './types/types.module';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     TypesModule,
-    CatsModule
+    CatsModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -20,6 +22,6 @@ export class AppModule implements NestModule{
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes('cats');
+      .forRoutes('cats', 'auth');
   }
 }
